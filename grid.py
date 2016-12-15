a0e3efd6b848c1f2d7b99efbe1ff548e9b8516f4
Sarita Upadhyay <sarita.upadhyay@gmail.com>
2016-12-14 16:31:26 -0800

    grid as a class

--- grid.py
+++ grid.py
@@ -1,47 +1,53 @@
 import sys
 
+num_cells = 18
 
-grid ={}
-for x in range(1, 11):
-	for y in range(1, 11):
-		grid[(x,y)] = " "
+class Grid(object):
+	def __init__ (self, num_cells=18):
+		self.num_cells = num_cells
+		self.grid = {}
+		# pre-populate the grid w/ 2 nested for loops
+		for x in range(1, num_cells+1):
+			for y in range(1, num_cells+1):
+				self.grid[(x,y)] = " "
 
-def make_grid_line():
-	line = ""
-	for y in range(10):
-		line = line + "+---------" 
-	return line + "+ \n"
+	def make_grid_line(self):
+		line = ""
+		for y in range(num_cells):
+			line = line + "+---------" 
+		return line + "+ \n"
 
-def get_coordinate_value(x, y):
-	if grid[(x,y)] == " ":
-		return "| (%s,%s) " % (format_number(x), format_number(y))
-	elif grid[(x,y)] == "b":
-		return "|    b    "
-	elif grid[(x,y)] == "w":
-		return "|    w    "
-	else:
-		return "error"
-
-
-def format_number(num):
-	if num <10:
-		return " %i" %num
-	else:
-		return num
-
-def make_grid():
-	for y in range(1, 11):
-		sys.stdout.write(make_grid_line())	
-		for x in range(1, 11):
-			sys.stdout.write (get_coordinate_value(x, y))
-			if x == 10:
-				sys.stdout.write("|\n")
-	sys.stdout.write(make_grid_line())			
-
-make_grid()
+	def get_coordinate_value(self,x, y):
+		if self.grid[(x,y)] == " ":
+			return "| (%s,%s) " % (self.format_number(x), self.format_number(y))
+		elif self.grid[(x,y)] == "b":
+			return "|    b    "
+		elif self.grid[(x,y)] == "w":
+			return "|    w    "
+		else:
+			return "error"
 
 
-class Players(object):
+	def format_number(self,num):
+		if num <10:
+			return " %i" %num
+		else:
+			return num
+
+	def make_grid(self):
+		for y in range(1, num_cells+1):
+			sys.stdout.write(self.make_grid_line())	
+			for x in range(1, num_cells+1):
+				sys.stdout.write (self.get_coordinate_value(x, y))
+				if x == num_cells:
+					sys.stdout.write("|\n")
+		sys.stdout.write(self.make_grid_line())			
+
+make_grid = Grid()
+make_grid.make_grid()
+
+
+class Players(Grid):
 	def __init__(self):
 		self.black_player = "Black Player"
 		self.white_player = "White Player"
@@ -79,13 +85,13 @@ class PlayGame(Players):
 		move_input_y = self.parse_input("y coordinate: ")
 		print "You have chosen spot (%s,%s)" % (move_input_x, move_input_y)
 		if self.valid_move(move_input_x, move_input_y) == move_input_x:
-			if grid[(move_input_x,move_input_y)] == " ":
-				grid[(move_input_x, move_input_y)] = self.player_name_abbr()
+			if self.grid[(move_input_x,move_input_y)] == " ":
+				self.grid[(move_input_x, move_input_y)] = self.player_name_abbr()
 				self.turn_count = self.turn_count + 1
 			else:
 				print "ERROR: There is already a piece in that location."
-				make_grid()
-		make_grid()
+				self.make_grid()
+		self.make_grid()
 
 	def parse_input(self, question):
 		input = raw_input(question)
@@ -95,14 +101,14 @@ class PlayGame(Players):
 			return int(input)
 		except ValueError:
 			print "ERROR: Not a valid input. Please type an integer that appears on the board."
-			self.parse_input(question)
+			return self.parse_input(question)
 		else:
 			return int(input)
 			
 
 	def valid_move(self, x,y):
 		try:
-			grid[(x, y)]
+			self.grid[(x, y)]
 			return x
 		except KeyError:
 			print "ERROR: Move must be within the grid"
